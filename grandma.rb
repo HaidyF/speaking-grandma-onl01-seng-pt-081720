329a41f5befc0c260e51c6a1c8c833a443199771
Haidy  Farahat <haidyfarahat@gmail.com>
2020-08-19 14:42:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,6 +19,10 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "Hi!"
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
+    
+    
 
 
   end
